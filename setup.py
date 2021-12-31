268c1b1400d35065ae11377690471aefd73b210f
vidargit <96905064+vidargit@users.noreply.github.com>
2021-12-31 17:02:09 +0900

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+setup(name='stack',
+
+version='0.0.1',
+
+description='Stack class',
+
+author='2gis',
+
+author_email='vidargit@gmail.com',
+
+url=None,
+
+license='MIT',
+
+py_modules=None,
+
+python_requires='>=3',
+
+packages=['stack']
+
+)
